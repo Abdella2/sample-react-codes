78ba5f00e0d9f3f6c99713fd1e5bbbd939d506c9
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-11-06 14:45:20 +0300

    Render jsx object(virtual DOM) into the real DOM.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import ReactDOM from 'react-dom';
 const element = <h1>Hello world!!!</h1>;
 console.log(element);
 
 console.log(<h2>another hello world</h2>);
+
+ReactDOM.render(element, document.getElementById('root'));
