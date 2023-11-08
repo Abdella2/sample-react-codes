0f428dd47bd951fe941693399945d1e72ed12b24
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-11-08 19:14:56 +0300

    Create cart component.

---
+++ shopping_cart/src/components/cart.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class Cart extends Component {
+  render() {
+    return <h1>Well Come to shopping cart</h1>;
+  }
+}
+
+export default Cart;
--- shopping_cart/src/index.js
+++ shopping_cart/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Cart from './components/cart';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Cart />
   </React.StrictMode>
 );
 
