e535859db07ebc40b5760aab286d3ae7756a4381
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-11-12 20:52:04 +0300

    Build the employees table.

--- EMS/src/components/employees.jsx
+++ EMS/src/components/employees.jsx
@@ -1,7 +1,37 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
+import '../App.css';
+import { getEmployees } from '../services/fakeEmployeeService';
 
 export class Employees extends Component {
+  state = {
+    employees: getEmployees()
+  };
+
   render() {
-    return 'hello';
+    const { employees } = this.state;
+    return (
+      <Fragment>
+        <table className="table">
+          <thead className="thead">
+            <tr>
+              <th>Employee No.</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+            </tr>
+            <tbody>
+              {employees.map((e) => (
+                <tr>
+                  <td>{e.employeeNo}</td>
+                  <td>{e.name}</td>
+                  <td>{e.email}</td>
+                  <td>{e.gender.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </thead>
+        </table>
+      </Fragment>
+    );
   }
 }
