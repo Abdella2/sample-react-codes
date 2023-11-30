d8b69532f770979935713209e46690f4edceb898
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-11-30 10:22:34 +0300

    Add sort icon

--- EMS/src/index.js
+++ EMS/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './App.css';
 import { Employees } from './components/employees';
 import reportWebVitals from './reportWebVitals';
 
