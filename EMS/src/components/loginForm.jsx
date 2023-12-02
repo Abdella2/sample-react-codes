490935648ff2d5e9f8807544a7da336ac2eb4d29
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-02 14:48:02 +0300

    Link input fields to react state - change to controlled elements.

--- EMS/src/components/loginForm.jsx
+++ EMS/src/components/loginForm.jsx
@@ -1,19 +1,51 @@
 import { Component, Fragment } from 'react';
 
 class LoginForm extends Component {
+  state = {
+    account: { username: '', password: '' }
+  };
+  handleSave = (e) => {
+    e.preventDefault();
+    console.log('saving');
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+
+    this.setState({
+      account
+    });
+  };
   render() {
+    const { username, password } = this.state.account;
     return (
       <Fragment>
-        <h1>Login</h1>
-        <form>
+        <h1>account</h1>
+        <form onSubmit={this.handleSave}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              className="form-control"
+              value={username}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+              value={password}
+              onChange={this.handleChange}
+            />
           </div>
+          <button className="btn btn-primary">Save</button>
         </form>
       </Fragment>
     );
