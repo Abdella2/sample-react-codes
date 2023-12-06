c82579815ee79491c1de7161d697907c1ae14bf2
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-06 15:29:37 +0300

    Implement post form validation.

--- http-app/src/components/postForm.jsx
+++ http-app/src/components/postForm.jsx
@@ -1,17 +1,47 @@
 import axios from 'axios';
+import Joi from 'joi-browser';
 import { Component, Fragment } from 'react';
 import withRouter from './hoc/withRouter';
 
 class PostForm extends Component {
   state = {
-    post: { title: '', body: '' }
+    post: { title: '', body: '' },
+    errors: {}
+  };
+
+  schema = {
+    title: Joi.string().required().label('Title'),
+    body: Joi.string().required().label('Body')
+  };
+
+  validateInput = (input) => {
+    const partialPost = { [input.name]: input.value };
+    const schema = { [input.name]: this.schema[input.name] };
+
+    const { error } = Joi.validate(partialPost, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const error = this.validateInput(input);
+    error ? (errors[input.name] = error) : delete errors[input.name];
+
     const post = { ...this.state.post };
     post[input.name] = input.value;
 
-    this.setState({ post });
+    this.setState({ post, errors });
+  };
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.post, this.schema, options);
+
+    const errors = {};
+    if (error)
+      error.details.map((error) => (errors[error.path] = error.message));
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = async (e) => {
@@ -19,6 +49,12 @@ class PostForm extends Component {
 
     const { post } = this.state;
     const { navigate } = this.props;
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+
+    if (errors) return;
+
     try {
       await axios.post('https://jsonplaceholder.typicode.com/posts', post);
       return navigate('/posts');
@@ -28,7 +64,7 @@ class PostForm extends Component {
     }
   };
   render() {
-    const { post } = this.state;
+    const { post, errors } = this.state;
     return (
       <Fragment>
         <h1>New Post</h1>
@@ -43,6 +79,9 @@ class PostForm extends Component {
               value={post.title}
               onChange={this.handleChange}
             />
+            {errors.title && (
+              <div className="alert alert-danger">{errors.title}</div>
+            )}
           </div>
           <div className="form-group">
             <label htmlFor="body">Body</label>
@@ -54,8 +93,13 @@ class PostForm extends Component {
               value={post.body}
               onChange={this.handleChange}
             />
+            {errors.body && (
+              <div className="alert alert-danger">{errors.body}</div>
+            )}
           </div>
-          <button className="btn btn-primary">Save</button>
+          <button className="btn btn-primary" disabled={this.validate()}>
+            Save
+          </button>
         </form>
       </Fragment>
     );
