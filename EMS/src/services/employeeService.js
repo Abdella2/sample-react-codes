869f8b77eba5f676b846a777adbaad6d4885d455
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-10 11:59:50 +0300

    Extract employeeService url to a function.

--- EMS/src/services/employeeService.js
+++ EMS/src/services/employeeService.js
@@ -6,16 +6,22 @@ const apiEndpoint = `${config.baseUrl}/employees`;
 export function getEmployees() {
   return http.get(apiEndpoint);
 }
-export function getEmployee(id) {
-  return http.get(`${apiEndpoint}/${id}`);
+
+function employeeUrl(id) {
+  return `${apiEndpoint}/${id}`;
 }
+
+export function getEmployee(id) {
+  return http.get(employeeUrl(id));
+}
+
 export function saveEmployee(employee) {
   if (employee._id) {
     delete employee._id;
-    return http.put(`${apiEndpoint}/${employee.id}`, employee);
+    return http.put(employeeUrl(employee.id), employee);
   }
   return http.post(apiEndpoint, employee);
 }
 export function deleteEmployee(employeeId) {
-  return http.delete(`${apiEndpoint}/${employeeId}`);
+  return http.delete(employeeUrl(employeeId));
 }
